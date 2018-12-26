43648265d42ab0ff528d20be8db3b37591126e7c
Ibrahim Dedeic <idedeic1@etf.unsa.ba>
2018-12-26 16:17:37 +0100

    tut+

--- src/ba/unsa/etf/rpr/GUIController.java
+++ src/ba/unsa/etf/rpr/GUIController.java
@@ -1,9 +1,38 @@
 package ba.unsa.etf.rpr;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.util.Locale;
+import java.util.ResourceBundle;
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class GUIController {
+    public BorderPane glavna;
+
+    private void selectLanguage(Locale locale) {
+
+
+        Stage primaryStage = (Stage)glavna.getScene().getWindow();
+        Locale.setDefault(locale);
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI.fxml"), bundle);
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setMinWidth(600);
+        primaryStage.setMinHeight(400);
+    }
     public void bosanski(ActionEvent actionEvent){
         Locale.setDefault(new Locale("bs", "BA"));
     }
@@ -11,9 +40,10 @@ public class GUIController {
         Locale.setDefault(new Locale("en", "US"));
     }
     public void njemacki(ActionEvent actionEvent){
-        Locale.setDefault(new Locale("de", "BA"));
+        Locale.setDefault(new Locale("de", "DE"));
     }
     public void francuski(ActionEvent actionEvent){
-        Locale.setDefault(new Locale("fr", "BA"));
+        Locale.setDefault(new Locale("fr", "FR"));
     }
+
 }
